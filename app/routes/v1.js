0a7127c6d56599f1f297a11a442f30f90a83fbfd
Michael Ashinze <34244307+mikeash82@users.noreply.github.com>
2022-04-27 11:20:37 +0100

    changes

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -9,27 +9,69 @@ router.use('/node_modules', express.static('node_modules'))
 
 
 
-router.post(`/v${verNum}/question_1-branch`, function (req, res) {
+router.post(`/v1/question_1-branch`, function (req, res) {
     const checkGias = req.session.data['question_1-gias'];
 
     if (checkGias === 'No') {
-        res.redirect(`/v${verNum}/question_2`);
+        res.redirect(`/v1/question_2`);
     } else {
-        res.redirect(`/v${verNum}/approved`);
+        res.redirect(`/v1/approved`);
     }
 
 
 
 });
 
+router.post(`/v2/question_1-branch`, function (req, res) {
+    const checkGias = req.session.data['question_1-gias'];
 
-router.post(`/v${verNum}/question_4-branch`, function (req, res) {
+    if (checkGias === 'Yes') {
+        res.redirect(`/v2/approved`);
+      } else if (checkGias === 'No'){
+            res.redirect(`/v2/approved`);
+    } else {
+        res.redirect(`/v2/question_2`);
+    }
+
+
+
+});
+
+router.post(`/v2/question_5-branch`, function (req, res) {
+    const checkGias = req.session.data['question_1-gias'];
+
+    if (checkGias === 'Yes') {
+        res.redirect(`/v2/approved`);
+      } else if (checkGias === 'No'){
+            res.redirect(`/v2/bluecoat-primary-agreememt-rejected`);
+    } else {
+        res.redirect(`/v2/bluecoat-primary-agreememt-waiting`);
+    }
+
+
+
+});
+
+router.post(`/v1/question_4-branch`, function (req, res) {
     const checkGias = req.session.data['question_4-gias'];
 
     if (checkGias === 'No') {
-        res.redirect(`/v${verNum}/bluecoat-primary-agreememt-rejected`);
+        res.redirect(`/v1/bluecoat-primary-agreememt-rejected`);
     } else {
-        res.redirect(`/v${verNum}/bluecoat-primary-agreememt-waiting`);
+        res.redirect(`/v1/bluecoat-primary-agreememt-waiting`);
+    }
+
+
+
+});
+
+router.post(`/v2/question_4-branch`, function (req, res) {
+    const checkGias = req.session.data['question_4-gias'];
+
+    if (checkGias === 'No') {
+        res.redirect(`/v2/bluecoat-primary-agreememt-rejected`);
+    } else {
+        res.redirect(`/v2/bluecoat-primary-agreememt-waiting`);
     }
 
 
--- app/routes/v2.js
+++ app/routes/v2.js
@@ -9,20 +9,35 @@ router.use('/node_modules', express.static('node_modules'))
 
 
 
-router.post(`/v${verNum}/question_3-branch`, function (req, res) {
-    const checkGias = req.session.data['question_2-gias'];
+router.post(`/v${verNum}/question_1-branch`, function (req, res) {
+    const checkGias = req.session.data['question_1-gias'];
 
     if (checkGias === 'No') {
-        res.redirect(`/v${verNum}/question_3`);
+        res.redirect(`/v${verNum}/question_2`);
+    } else {
+        res.redirect(`/v${verNum}/approved`);
     } else {
         res.redirect(`/v${verNum}/approved`);
     }
 
 
 
+
 });
 
 
+router.post(`/v${verNum}/question_4-branch`, function (req, res) {
+    const checkGias = req.session.data['question_4-gias'];
+
+    if (checkGias === 'No') {
+        res.redirect(`/v${verNum}/bluecoat-primary-agreememt-rejected`);
+    } else {
+        res.redirect(`/v${verNum}/bluecoat-primary-agreememt-waiting`);
+    }
+
+
+
+});
 
 
 // no code below here
